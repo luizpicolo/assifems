d47327c1df941c7b6c2b20c1810515c05beda592
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-21 19:35:16 -0400

    fix(establishments): fixed include category in filtered establishment

--- app/controllers/establishments_controller.js
+++ app/controllers/establishments_controller.js
@@ -1,24 +1,21 @@
-const { Establishment } = require('../models');
-const { Category } = require('../models');
+const { Establishment } = require("../models");
+const { Category } = require("../models");
 
-
-exports.list = async function(req, res) {
-
+exports.list = async function (req, res) {
   const establishments = await Establishment.findAll({
-    include: 'category'
+    include: "category",
   });
   const categories = await Category.findAll();
-  res.render('establishments', {establishments, categories});
+  res.render("establishments", { establishments, categories });
 };
 
-exports.filter = async function(req, res) {
-
-
-  const establishments = await Establishment.findAll(
-   { where: {categoryId: Object.values(req.body.category)}}
-  );
+exports.filter = async function (req, res) {
+  const establishments = await Establishment.findAll({
+    where: { categoryId: Object.values(req.body.category) },
+    include: "category",
+  });
   const categories = await Category.findAll();
-  res.render('establishments', {establishments, categories});
+  res.render("establishments", { establishments, categories });
 };
 
 // exports.find = async function(req, res) {
