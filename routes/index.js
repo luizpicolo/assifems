f91f04edb81fa615f7f97dcbb6d5e3f834e18445
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-10-21 21:13:08 -0400

    fix(associates): fixed createdAt and updatedAt column

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { Associate } = require("../app/models");
+var isLogged = false;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  if (isLogged) {
+    res.render("index");
+  } else {
+    res.render("login");
+  }
+});
+
+router.post("/", async function (req, res, next) {
+  const associate = await Associate.findOne({ where: { cpf: req.body.cpf } });
+
+  if (associate != null) {
+    isLogged = true;
+    res.render("index", { associate });
+  } else {
+    res.render("login");
+  }
+});
+
+router.post("/deslogar", async function (req, res, next) {
+  isLogged = false;
 });
 
 module.exports = router;
