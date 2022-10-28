3067c4e10a4c1c1a746fec6d7056e70099cc084b
Luiz F. Picolo <luizpicolo@gmail.com>
2022-10-27 20:52:59 -0400

    Adicionados pré-compiladores sass

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
 const establishmentsRouter = require('./routes/establishments');
@@ -20,6 +21,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true,
+  outputStyle: 'compressed'
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
