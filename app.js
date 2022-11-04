eb33f3a4606a1c08ebe27c3cf4668e58da892878
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-03 21:47:20 -0400

    feat: setup jwt

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // JWT
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
-const express = require("express");
+// JWT
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+
 const router = express.Router();
 const { Associate } = require("../app/models");
-var cookieParser = require('cookie-parser')
+
+require("dotenv/config");
+const jwt = require('jsonwebtoken');
+var { expressjwt: expressJWT } = require("express-jwt");
+
 // GLOBAL VARS
 var isLogged = false;
 var associate = null;
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -21,6 +33,11 @@ router.post("/", async function (req, res, next) {
   });
 
   if (findAssociate != null) {
+    const id = 1;
+    const token = jwt.sign({ id }, process.env.SECRET, {
+    expiresIn: 3600, // expires in 1 hour
+    });
+    res.cookie("token", token, { httpOnly: true });
     isLogged = true;
     associate = findAssociate;
     res.render("index", { associate });
@@ -31,6 +48,7 @@ router.post("/", async function (req, res, next) {
 
 router.post("/deslogar", async function (req, res, next) {
   isLogged = false;
+  res.cookie("token", null, { httpOnly: true });
   res.render("login", {error: false});
 });
 
