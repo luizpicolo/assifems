9827407234cd48aa3d08232ef537410862ce57b3
andredochute <andredochute@gmail.com>
2022-11-24 21:39:33 -0400

    feat(menu): add animations

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,20 @@
+//sw
+window.onload = () => {
+  "use strict";
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+  }
+  
+  document.querySelector('#show').addEventListener('click', () => {
+    const iconUrl = document.querySelector('select').selectedOptions[0].value;
+    let imgElement = document.createElement('img');
+    imgElement.src = iconUrl;
+    document.querySelector('#container').appendChild(imgElement);
+  });
+};
+
 //imports
+const body = document.querySelector('body')
 const returnMenu = document.getElementById("return");
 const resp_options = document.getElementById("more_options");
 const icon = document.getElementById("arrow");
@@ -10,14 +26,33 @@ var visibility = true;
 
 function menu() {
   if (visibility) {
-    resp_options.style.display = "block";
+    
+    if(window.outerWidth < 600){
+    resp_options.style.left = 0;
+    }else{
+      resp_options.style.transition = 'height 1s';
+      resp_options.style.height = '400px';
+    }
+    resp_options.style.opacity = 1;
+    
+    body.style.overflow = 'hidden';
+    
+     
     icon.style.cssText = `
     transform: rotateX(180deg);
     transition: 500ms;
     `;
     visibility = false;
   } else {
-    resp_options.style.display = "none";
+    if(window.outerWidth < 600){
+      resp_options.style.left = '-100%';
+      }else{
+        
+        resp_options.style.height = '0px';
+      }
+    resp_options.style.transition = '1s';
+    resp_options.style.opacity = 0;
+    body.style.overflow = 'unset';
     visibility = true;
     icon.style.cssText = `
     transform: rotateX(0deg);
