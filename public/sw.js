d5080ce9c691879365002217df8c327e7c653844
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-29 22:05:36 -0400

    feat: add establishments router to precache

--- public/sw.js
+++ public/sw.js
@@ -19,7 +19,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  '/',
+  '/', '/estabelecimentos'
 ];
 
 // The install handler takes care of precaching the resources we always need.
